bd5ceb42443149cf88c69e23e2d329f3054a6e62
amaralvin7 <amaralvin7@gmail.com>
2022-02-18 13:59:52 -0800

    Retrain InceptionV3 with a subest of particle image data
    Mostly following "FINETUNING TORCHVISION MODELS" and "TRANSFER LEARNING FOR COMPUTER VISION" PyTorch tutorials. Data used were 200 particle images (100 aggregates and 100 amphipods). The only data transforms were resize, center crop, and normalization, following the InceptionV3 PyTorch homepage. Targets (labels) not transformed. Adam optimizer. Todo's include stratifying train/validation split, looking into data augmentation, and figuring out whether or not I need to transform the targets. Current validation accuracy reaches close to 100%. I also need to look into whether this is real or overfitting.

--- pico/retrain.py
+++ pico/retrain.py
@@ -1,23 +1,109 @@
-import os
+# Adapted from https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html
+# kept only parts relevant to using inception v3 as a feature extractor
+
+import time
+import copy
+
 import torch
-from torchvision import datasets, models
-from torchvision.transforms import ToTensor, Lambda
+import torch.nn as nn
+from torchvision import models
+from tqdm import tqdm
 
-labels = [name for name in os.listdir(f'./data/by_label/') if os.path.isdir(f'./data/by_label/{name}')]
-n_labels = len(labels)
+def train_model(model, dataloaders, criterion, optimizer, num_epochs=25):
+    since = time.time()
 
-transform = ToTensor()
-target_transform = Lambda(
-    lambda y: torch.zeros(
-        n_labels, dtype=torch.float).scatter_(dim=0, index=torch.tensor(y),
-                                              value=1))
+    train_acc_history = []
+    val_acc_history = []
 
-dataset = datasets.DatasetFolder('./data/by_label/',
-                                 loader = datasets.folder.default_loader,
-                                 extensions = ('.jpg',),
-                                 transform=transform,
-                                 target_transform=target_transform)
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
 
-print(dataset[0])
+    for _ in tqdm(range(num_epochs), desc='Epoch'):
+        # print(f'Epoch {epoch}/{num_epochs - 1}')
+        # print('-' * 10)
 
-                                 
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                # inputs = inputs.to(device)
+                # labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    if phase == 'train':
+                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                        outputs, aux_outputs = model(inputs)
+                        loss1 = criterion(outputs, labels)
+                        loss2 = criterion(aux_outputs, labels)
+                        loss = loss1 + 0.4*loss2
+                    else:
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            # epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = (running_corrects.double()
+                         / len(dataloaders[phase].dataset))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc.cpu().numpy())
+            else:
+                train_acc_history.append(epoch_acc.cpu().numpy())
+
+    time_elapsed = time.time() - since
+    print(f'Training complete in {time_elapsed//60:.0f}m {time_elapsed%60:.0f}s')
+    print(f'Best val Acc: {best_acc:4f}')
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history, train_acc_history
+
+def initialize_model(num_classes):
+    """ Inception v3
+    Be careful, expects (299,299) sized images and has auxiliary output
+    """
+    model = models.inception_v3(pretrained=True)
+    for param in model.parameters():
+        param.requires_grad = False
+        
+    # Handle the auxilary net
+    num_features_aux = model.AuxLogits.fc.in_features
+    model.AuxLogits.fc = nn.Linear(num_features_aux, num_classes)
+    
+    # Handle the primary net
+    num_features_main = model.fc.in_features
+    model.fc = nn.Linear(num_features_main, num_classes)
+
+    return model
+
---
+++ pico/run.py
@@ -0,0 +1,80 @@
+"""
+todo
+- stratified splits
+- transforms (data augmentation?)
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+import retrain
+
+# # Detect if we have a GPU available
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+batch_size = 16
+num_workers = 0
+num_epochs = 100
+input_size = 299
+
+# Define the preprocessing transforms
+# https://pytorch.org/hub/pytorch_vision_inception_v3/
+preprocess = transforms.Compose([
+    transforms.Resize(299),
+    transforms.CenterCrop(299),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225]),])
+
+# load the full dataset
+data_dir = "./smalldata"
+full_dataset = datasets.ImageFolder(data_dir, preprocess)
+
+# train/val split (non-stratified)
+train_size = int(0.8 * len(full_dataset))
+val_size = len(full_dataset) - train_size
+train_dataset, val_dataset = torch.utils.data.random_split(
+    full_dataset, [train_size, val_size])
+
+# create dataloaders for the train/val datasets
+img_datasets = {'train': train_dataset, 'val': val_dataset}
+dataloaders_dict = {k: DataLoader(
+    v, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+                    for k, v in img_datasets.items()}
+
+# initialize the model
+num_classes = len(full_dataset.classes)
+model = retrain.initialize_model(num_classes)
+# model = model.to(device)
+
+# figure out which params we have to update
+params_to_update = []
+for name, param in model.named_parameters():
+    if param.requires_grad == True:
+        params_to_update.append(param)
+
+optimizer = optim.Adam(params_to_update)
+criterion = nn.CrossEntropyLoss()
+
+#retrain the model
+model, vhist, thist = retrain.train_model(
+    model, dataloaders_dict, criterion, optimizer, num_epochs=num_epochs)
+
+# plot training and validation accuracy histories
+plt.title("Accuracy vs. Number of Training Epochs")
+plt.xlabel("Training Epochs")
+plt.ylabel("Accuracy")
+plt.plot(range(1,num_epochs+1),vhist,label="validation")
+plt.plot(range(1,num_epochs+1),thist,label="training")
+plt.ylim((0,1.))
+plt.xticks(np.arange(1, num_epochs+1, 1.0))
+plt.ylim([-0.1, 1.1])
+plt.legend()
+plt.savefig('history')
+plt.close()
+    
