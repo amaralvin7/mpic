8b0baeed660172860c1d38c2c3f9193bcff62af6
amaralvin7 <amaralvin7@gmail.com>
2022-09-26 15:20:25 -0700

    Compare CD's revisions of VA's re-labels

--- label_comparison.py
+++ label_comparison.py
@@ -9,46 +9,34 @@ from sklearn.metrics import ConfusionMatrixDisplay
 
 from preprocess import make_dir
 
-df = pd.read_csv('Compared_classification.csv')
-df['CD_label2_grouped'] = df['CD_label2']
+def labels_to_df(path, prefix):
 
-# CD's labels to be grouped
-swimmer = ('zooplankton', 'zooplankton_part', 'copepod', 'amphipod', 'pteropod', 'foraminifera')
-long_pellet = ('long_fecal_pellet', 'large_loose_pellet')
-
-# VA's groups to be grouped
-unidentifiable = ('skip', 'noise', 'bubble')
-fiber = ('fiber_sharp', 'fiber_blur')
-phytoplankton = ('phyto_round', 'phyto_long')
-
-df.loc[df['CD_label2'].isin(swimmer), 'CD_label2_grouped'] = 'swimmer'
-df.loc[df['CD_label2'] == 'dense_detritus', 'CD_label2_grouped'] = 'aggregate'
-df.loc[df['CD_label2'].isin(long_pellet), 'CD_label2_grouped'] = 'long_pellet'
+    labels = [l for l in os.listdir(path) if os.path.isdir(os.path.join(path, l))]
+    list_of_labels = []
+    list_of_filenames = []
+    for l in labels:
+        filenames = [f for f in os.listdir(os.path.join(path, l)) if '.jpg' in f]
+        list_of_filenames.extend(filenames)
+        list_of_labels.extend([l] * len(filenames))
+    df = pd.DataFrame(list(zip(list_of_filenames, list_of_labels)), columns =['file', f'{prefix}_label'])
+    
+    return df
 
 # put VA's labels into a df, and merge them with CD's
-relabel_path = '/Users/particle/imgs/relabel/finish'
-labels = [l for l in os.listdir(relabel_path) if os.path.isdir(os.path.join(relabel_path, l))]
-list_of_labels = []
-list_of_filenames = []
-for l in labels:
-    filenames = [f for f in os.listdir(os.path.join(relabel_path, l)) if '.jpg' in f]
-    list_of_filenames.extend(filenames)
-    list_of_labels.extend([l] * len(filenames))
-df2 = pd.DataFrame(list(zip(list_of_filenames, list_of_labels, list_of_labels)), columns =['file', 'VA_label', 'VA_label_grouped'])
-df2.loc[df2['VA_label_grouped'].isin(unidentifiable), 'VA_label_grouped'] = 'unidentifiable'
-df2.loc[df2['VA_label_grouped'].isin(fiber), 'VA_label_grouped'] = 'fiber'
-df2.loc[df2['VA_label_grouped'].isin(phytoplankton), 'VA_label_grouped'] = 'phytoplankton'
-df2 = df2[df2['VA_label_grouped'] != 'unidentifiable']  # drop VA's unidentifiables
+va_path = '/Users/particle/imgs/relabel_20220926'
+df = labels_to_df(va_path, 'VA')
+df2 = labels_to_df('/Users/particle/imgs/relabel_20220920_CD', 'CD')
+
 df = df.merge(df2, on='file')
 
 #calculate agreement percentage between CD and VA, and calculate the confusion matrix
-agreement = sum(df['CD_label2_grouped'] == df['VA_label_grouped'])
-print(f"Number of VA's identifiable images labeled by CD: {len(df)}")
-print(f'Agreement percentage: {agreement/len(df)*100:.0f} (N = {agreement})')
+agreement = sum(df['CD_label'] == df['VA_label'])
+print(f'Agreement percentage: {agreement/len(df)*100:.2f} (N = {agreement})')
+
 fig, ax = plt.subplots(figsize=(10,10))
 disp = ConfusionMatrixDisplay.from_predictions(
-    y_true=df['CD_label2_grouped'],
-    y_pred=df['VA_label_grouped'],
+    y_true=df['CD_label'],
+    y_pred=df['VA_label'],
     # display_labels=test_data.classes,
     cmap=plt.cm.Blues,
     normalize=None,
@@ -64,11 +52,10 @@ plt.close()
 
 # copy disagreements into a folder
 make_dir('disagreements')
-disagreements = df.loc[df['CD_label2_grouped'] != df['VA_label_grouped']]
+disagreements = df.loc[df['CD_label'] != df['VA_label']]
 for _, row in disagreements.iterrows():
-    cd_label = row['CD_label2_grouped']
-    va_label_grouped = row['VA_label_grouped']
-    va_label_ungrouped = row['VA_label']
+    cd_label = row['CD_label']
+    va_label = row['VA_label']
     file = row['file']
-    shutil.copy(os.path.join(relabel_path, va_label_ungrouped, file),
-                os.path.join('disagreements', f'{file.split(".")[0]}_CD_{cd_label}_VA_{va_label_ungrouped}.jpg'))
+    shutil.copy(os.path.join(va_path, va_label, file),
+                os.path.join('disagreements', f'{file.split(".")[0]}_CD_{cd_label}_VA_{va_label}.jpg'))
